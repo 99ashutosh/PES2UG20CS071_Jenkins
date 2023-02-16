6385984665d84e69632e89dbd1c9d727373c9ce8
99ashutosh <ashutosh.na.99@gmail.com>
2023-02-16 19:45:49 +0530

    Added name

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "SRN: PES2UG20CS071\n";
+  cout << "Name: Ashutosh\n SRN: PES2UG20CS071\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
